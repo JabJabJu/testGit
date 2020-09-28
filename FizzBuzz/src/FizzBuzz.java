121e878be28ad4a4e6eea0d69a9e313350c853f8
Julien <hf691143@ens.uqam.ca>
2020-09-27 22:01:17 -0400

    Modifié la classe FizzBuzz afin de compter jusqu'à 200 au lieu de 100

--- FizzBuzz/src/FizzBuzz.java
+++ FizzBuzz/src/FizzBuzz.java
@@ -2,16 +2,16 @@
  * Petit programme FizzBuzz. On compte jusqu'a un nombre predetermine d'iterations
  * et on remplace les chiffres qui sont des multiples de 3 par Fizz et ceux qui
  * sont des multiples de 5 par Buzz.
- * Exemple : 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz ...
+ * Exemple : 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz ...
  */
 
 public class FizzBuzz {
 
     public static void main(String[] args) {
-        int iterations = 100;
+        int iterations = 200;
 
         for (int i = 1; i < iterations + 1; i++) {
-            if (i % 3 != 0 && i % 5 !=0) {
+            if (i % 3 != 0 && i % 5 != 0) {
                 System.out.print(i);
             } else {
                 if (i % 3 == 0)
